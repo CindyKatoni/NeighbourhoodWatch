96810d20e1b27d8ed1d8cc4a90454456e1fc3323
CindyKatoni <katmuema@gmail.com>
2022-02-04 03:30:37 +0300

    Register models in admin file.

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post, Business, Authority, HealthService
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Authority)
+admin.site.register(HealthService)
